92f008785298d1b585098e22a40ddd8af7647ab7
ziniFilippo <101709141+ziniFilippo@users.noreply.github.com>
2024-04-20 07:54:57 +0000

    added base frontend and get api

--- src/Controllers/CapController.php
+++ src/Controllers/CapController.php
@@ -1,6 +1,8 @@
 <?php
     namespace App\Controllers;
-
+    
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Container\ContainerInterface;
 
     class CapController {
@@ -8,10 +10,23 @@
         protected $container;
 
         public function __construct(ContainerInterface $container){
-//            $this->db =  new \PDO('mysql:host=localhost;dbname=PM', 'root', '');
+            $this->db =  new \PDO('mysql:host=localhost;dbname=CODPOST', 'root', '');
             $this->container = $container;
-            $this->db = new \PDO(
-                $container
-            )
+        }
+        public function getCaps(Response $response){
+            $qry = "SELECT * FROM CODICE";
+            $stmt = $this->db->prepare($qry);
+            $stmt->execute();
+            $caps = $stmt->fetchAll();
+            return $response->withJson($caps);
+        }
+        public function addCap(Request $request, Response $response){
+
+        }
+        public function delCap(Request $request, Response $response){
+
+        }
+        public function updateCap(Request $request, Response $response){
+
         }
     }
--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,29 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 
+$app->get('', function (Response $response) {
+    $response->getBody()->write(file_get_contents('index.html'));
+    return $response;
+});
 
-$app-> get   ('api/caps/', [\App\Controllers\CapController::class,'getCaps']);
-$app-> post  ('api/cap', [\App\Controllers\CapController::class, 'addCap']);
+$app->get('api/get', function (Response $response) {
+    $response->getBody()->write(file_get_contents('api/get.html'));
+    return $response;
+});
+$app->get('api/add', function (Response $response) {
+    $response->getBody()->write(file_get_contents('api/addForm.html'));
+    return $response;
+});
+$app->get('api/delete', function (Response $response) {
+    $response->getBody()->write(file_get_contents('api/deleteForm.html'));
+    return $response;
+});
+$app->get('api/update', function (Response $response) {
+    $response->getBody()->write(file_get_contents('api/updateForm.html'));
+    return $response;
+});
+
+$app-> get('api/caps', [\App\Controllers\CapController::class,'getCaps']);
+$app-> post('api/cap', [\App\Controllers\CapController::class, 'addCap']);
 $app-> delete('api/cap/{id}',[\App\Controllers\CapController::class,'delCap']);
-$app-> put   ('api/cap/{id}',[\App\Controllers\CapController::class,'updateCap']);
+$app-> put('api/cap/{id}',[\App\Controllers\CapController::class,'updateCap']);
